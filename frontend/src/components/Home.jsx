7fa4d4abacf286ad224f8a442f1aac8a5455cc73
crazyay <ankuryadav5949@gmail.com>
2024-08-29 18:05:08 +0530

    folder-structure and tailwind config

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home(){
+    return (
+        <div>
+            <h1 className=" text-red-700 text-5xl" >Home Page</h1>
+        </div>
+    )
+}
+export default Home;
